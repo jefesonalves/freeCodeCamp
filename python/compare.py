f1db180aea3943f175f162f1a3e4e5b067136e19
jefesonalves <jefesonbezerra@gmail.com>
2023-09-18 23:10:15 -0300

    teste da primeira aula

---
+++ python/compare.py
@@ -0,0 +1,12 @@
+def compare():
+    x = int(input("x:"))
+    y = int(input("y:"))
+
+    if x > y:
+        print("x é maior do que y")
+    elif x < y:
+        print("x é menor do que y")
+    else:
+        print("x é igual a y")
+
+compare()
